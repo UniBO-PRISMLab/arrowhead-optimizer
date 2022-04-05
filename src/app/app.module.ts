a3c39f9fe83a4e7d9ab176eccd27aa59e524e6ee
ivan <idimitryz@gmail.com>
2022-04-05 22:32:02 +0200

    refactor: add components in the root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArrowheadService } from './services/arrowhead.service';
 import { DrHarvesterService } from './services/dr-harvester.service';
 import { ThingsService } from './services/things.service';
-import { TestServiceComponent } from './components/test-service/test-service.component';
+import { ObservableHandlerComponent } from './components/observable-handler/observable-handler.component';
+import { TestAsyncComponent } from './components/test-async/test-async.component';
+import { ArrowheadComponent } from './components/arrowhead/arrowhead.component';
+import { TestHarvesterComponent } from './components/test-harvester/test-harvester.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestServiceComponent
+    ObservableHandlerComponent,
+    TestAsyncComponent,
+    ArrowheadComponent,
+    TestHarvesterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [ArrowheadService, DrHarvesterService, ThingsService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
